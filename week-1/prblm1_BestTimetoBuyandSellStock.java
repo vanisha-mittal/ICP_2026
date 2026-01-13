2fd79f68c8af45540665a5ed78b19470ad6609af
vanisha-mittal <vanishamittal1409@gmail.com>
2026-01-13 11:05:56 +0530

    week1

---
+++ week-1/prblm1_BestTimetoBuyandSellStock.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] prices=new int[n];
+        for(int i=0;i<n;i++){
+            prices[i]=sc.nextInt();
+            
+        }
+    }
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
+            }
+        }
+        return profit;
+    }
+}
